3d58d4ff43d8db8e42e4072a3eab6520f2289675
Eduardo0504 <maujohernav50@gmail.com>
2025-06-20 17:23:49 -0600

    Interpolacion

--- variables/Program.cs
+++ variables/Program.cs
@@ -1,7 +1,9 @@
 ﻿var cantidad = 7;
 var cantidad2 = 9;
 var resultado = cantidad + cantidad2;
+var nombre = "Eduardo";
+//Console.WriteLine(" la suma de " + cantidad + " y " + cantidad2 + " es igual a: " + resultado);
 
-Console.WriteLine(" la suma de " + cantidad + " y " + cantidad2 + " es igual a: " + resultado);
+Console.WriteLine($"la suma de {cantidad} y {cantidad2} es igual a: {resultado}");
 
-
+Console.WriteLine($"mi nombre es {nombre.ToUpper()} en mayuscula");
