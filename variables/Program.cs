4377224ac7f9db7e7d7976b29d23dd68ed7578ea
Eduardo0504 <maujohernav50@gmail.com>
2025-06-20 16:14:12 -0600

    prueba

--- variables/Program.cs
+++ variables/Program.cs
@@ -1,5 +1,5 @@
 ﻿var cantidad = 7;
-var cantidad2 = 9;
+var cantidad2 = 10;
 var resultado = cantidad + cantidad2;
 
 Console.WriteLine("la suma de " + cantidad + " y " + cantidad2 + " es igual a: " + resultado);
