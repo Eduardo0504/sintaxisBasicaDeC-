0119718e76445c8afa4086a0a7fb9b6d10a53e17
Eduardo0504 <maujohernav50@gmail.com>
2025-06-20 17:43:28 -0600

    operaciones de asignacion

--- variables/Program.cs
+++ variables/Program.cs
@@ -1,9 +1,6 @@
 ﻿var cantidad = 7;
-var cantidad2 = 9;
+var cantidad2 = 25;
 var resultado = cantidad + cantidad2;
-var nombre = "Eduardo";
-//Console.WriteLine(" la suma de " + cantidad + " y " + cantidad2 + " es igual a: " + resultado);
 
-Console.WriteLine($"la suma de {cantidad} y {cantidad2} es igual a: {resultado}");
-
-Console.WriteLine($"mi nombre es {nombre.ToUpper()} en mayuscula");
+cantidad += cantidad2;
+Console.WriteLine(" la suma es " + resultado);
